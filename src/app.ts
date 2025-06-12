9f68bd143ae445260a23e1e99f75595846955b4d
MapleLeaf <197728340@qq.com>
2025-06-12 20:54:54 +0800

    refactor(app): 重构应用并添加启动日志
    - 更新 app.ts 中的 logger 中间件引用，使用绝对路径
    - 移除 app.ts 中的测试用简单中间件
    - 在 server.ts 中添加应用启动日志

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import koa from 'koa';
 import cors from '@koa/cors';
-import log from './middlewares/logger';
+import log from '@middlewares/logger';
 import PluginManager from '@lib/plugins/loader';
 
 const app = new koa();
@@ -10,8 +10,4 @@ app.use(cors)
 
 PluginManager.loadPlugins(app);  // 加载所有插件
 
-app.use(async (ctx, next) => {
-  console.log(`${ctx.method} ${ctx.url}`);
-  await next();
-});
 export default app;
