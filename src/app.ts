849e4c461a5b5fe2b196894f9055412ca09014e0
MapleLeaf <197728340@qq.com>
2025-06-12 21:29:41 +0800

    refactor(app): 更新应用中间件配置
    - 修改 logger 中间件的引用路径，使用绝对路径替代相对路径
    - 配置 cors 中间件，设置允许所有来源的跨域请求

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import koa from 'koa';
 import cors from '@koa/cors';
-import log from './middlewares/logger';
+import log from '@middlewares/logger';
 import PluginManager from '@lib/plugins/loader';
 
 const app = new koa();
 
 app.use(log);
 
-PluginManager.loadPlugins(app);  // 加载所有插件
+PluginManager.loadPlugins(app);
 
-app.use(cors)
+app.use(cors({
+    origin: '*',
+}));
 
 
 export default app;
